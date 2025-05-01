0b95f2146205f0cf02ae42aa37649fa13fd788be
Tarnvir-Virk <virktarnvir@gmail.com>
2025-04-30 19:01:59 -0700

    Update mtLEDBag.ino

--- mtLEDBag.ino
+++ mtLEDBag.ino
@@ -1,11 +1,15 @@
 
 int lightSensorPin = A2;
 
+int led12 = 12;
+
 int ledA5 = A5;
 int led6 = 6;
 
 void setup() {
   // put your setup code here, to run once:
+
+  pinMode(led12, OUTPUT);
 
   pinMode(ledA5, OUTPUT);
   pinMode(led6, OUTPUT);
@@ -15,25 +19,33 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if (analogRead(lightSensorPin) < 20) {
+  if (analogRead(lightSensorPin) < 30) {
+
+    digitalWrite(led12, HIGH);
 
     digitalWrite(ledA5, HIGH);
     digitalWrite(led6, HIGH);
 
   }
-  else if (analogRead(lightSensorPin) < 50) {
+  else if (analogRead(lightSensorPin) < 65) {
+
+    digitalWrite(led12, HIGH);
 
     digitalWrite(ledA5, HIGH);
     digitalWrite(led6, LOW);
 
   }
-  else if (analogRead(lightSensorPin) < 80) {
+  else if (analogRead(lightSensorPin) < 100) {
+
+    digitalWrite(led12, LOW);
 
     digitalWrite(ledA5, LOW);
     digitalWrite(led6, HIGH);
 
   }
   else {
+
+    digitalWrite(led12, LOW);
 
     digitalWrite(ledA5, LOW);
     digitalWrite(led6, LOW);
